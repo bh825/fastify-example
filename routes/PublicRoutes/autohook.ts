d2ec0028721cc639ad723e776f022c0e28a0b31f
Bharat Hirapara <97462006+bh825@users.noreply.github.com>
2024-09-09 17:46:49 +0530

    Delete routes/PublicRoutes/autohook.ts

--- routes/PublicRoutes/autohook.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from "fastify";
-
-export default async function (app: FastifyInstance) {
-  app.addHook("preHandler", async (req, reply) => {
-    console.log("public", req.body);
-  });
-}
